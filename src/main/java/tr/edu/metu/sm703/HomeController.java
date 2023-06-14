25f339727e96de75fbdc66611845a94f5eeb58f7
Erdem <35002668+cengerdem@users.noreply.github.com>
2023-06-15 00:15:19 +0300

    change variable name

--- src/main/java/tr/edu/metu/sm703/HomeController.java
+++ src/main/java/tr/edu/metu/sm703/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @Get
     public Map<String, Object> index() {
-        String password = "pass123";
-        return Collections.singletonMap("message", "Hello World new " + password);
+        String erdem = "sm703";
+        return Collections.singletonMap("message", "Hello World new " + erdem);
     }
 }
