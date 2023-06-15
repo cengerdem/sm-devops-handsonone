297f4e7e0c22eaa5c4303943b44a01bfb3066411
Erdem <35002668+cengerdem@users.noreply.github.com>
2023-06-15 03:18:30 +0300

    add vulnerable code

--- src/main/java/tr/edu/metu/sm703/HomeController.java
+++ src/main/java/tr/edu/metu/sm703/HomeController.java
@@ -4,11 +4,17 @@ import io.micronaut.http.annotation.Get;
 import java.util.Collections;
 import java.util.Map;
 
+import static java.security.spec.MGF1ParameterSpec.SHA256;
+
+
 @Controller
 public class HomeController {
 
     @Get
     public Map<String, Object> index() {
+        String inputString = "s3cr37";
+        byte[] key         = inputString.getBytes();
+
         String password = "sm703"; //TODO
         return Collections.singletonMap("message", "Hello World new " + password);
     }
