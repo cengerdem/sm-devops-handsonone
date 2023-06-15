ce6e82a6c0f77258288859dc033a8ba395a6ff9a
Erdem <35002668+cengerdem@users.noreply.github.com>
2023-06-15 03:26:56 +0300

    trigger pipeline

--- src/main/java/tr/edu/metu/sm703/HomeController.java
+++ src/main/java/tr/edu/metu/sm703/HomeController.java
@@ -4,7 +4,6 @@ import io.micronaut.http.annotation.Get;
 import java.util.Collections;
 import java.util.Map;
 
-import static java.security.spec.MGF1ParameterSpec.SHA256;
 
 
 @Controller
@@ -13,7 +12,7 @@ public class HomeController {
     @Get
     public Map<String, Object> index() {
         String inputString = "s3cr37";
-        byte[] key         = inputString.getBytes();
+        byte[] key         = inputString.getBytes(); //TODO
 
         String password = "sm703"; //TODO
         return Collections.singletonMap("message", "Hello World new " + password);
